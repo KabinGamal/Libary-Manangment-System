e5857ab0613e91c4d7c5d646d6227432242505e2
KabinGamal <nibak1223@gmail.com>
2026-04-06 20:48:17 +0545

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -9,3 +9,17 @@
 #include <iomanip>
 #include <limits>
 #include <regex>
+using namespace std;
+
+// Utility functions
+class DateUtils {
+public:
+    static string getCurrentDate() {
+        time_t now = time(0);
+        tm* localtm = localtime(&now);
+        stringstream ss;
+        ss << 1900 + localtm->tm_year << "-" 
+           << setw(2) << setfill('0') << (1 + localtm->tm_mon) << "-" 
+           << setw(2) << setfill('0') << localtm->tm_mday;
+        return ss.str();
+    }
